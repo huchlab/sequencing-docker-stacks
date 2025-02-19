2a5d6d1264bcf3dcdec888dd99a83b2f3903d390
Fabian Rost <rost@mpi-cbg.de>
2025-02-19 17:55:56 +0100

    Add unit tests for singlecell notebook

---
+++ tests/singlecell-notebook/units/unit_pca_scanpy.py
@@ -0,0 +1,18 @@
+import scanpy as sc
+
+adata = sc.datasets.pbmc3k()
+
+sc.pp.normalize_per_cell(adata)
+sc.pp.log1p(adata)
+sc.pp.pca(adata)
+
+X = adata.obsm['X_pca'].copy()
+
+
+for i in range(3):
+    print(i)
+    sc.pp.pca(adata)
+    try:
+        np.testing.assert_equal(X, adata.obsm['X_pca'])
+    except AssertionError:
+        raise AssertionError(f"Re-computed PCA for dense is not the same. Maximum absolute difference: {np.abs(X - adata.obsm['X_pca']).max()}")
