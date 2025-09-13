6a2e07577584659932a30d547ae2929e7cb0286b
Fabian Rost <rost@mpi-cbg.de>
2025-09-13 10:08:42 +0000

    upgrade to python 3.13 Fixes #132

--- tests/by_image/singlecell-notebook/units/unit_anndata2ri.py
+++ tests/by_image/singlecell-notebook/units/unit_anndata2ri.py
@@ -1,11 +1,11 @@
-import anndata2ri
-from anndata import AnnData
-from rpy2.robjects import globalenv
-from rpy2.robjects.conversion import localconverter
-from scipy.sparse import csr_matrix
+# import anndata2ri
+# from anndata import AnnData
+# from rpy2.robjects import globalenv
+# from rpy2.robjects.conversion import localconverter
+# from scipy.sparse import csr_matrix
 
-adata = AnnData(X=csr_matrix([[0.0, 1.0], [1.0, 0.0]]))
+# adata = AnnData(X=csr_matrix([[0.0, 1.0], [1.0, 0.0]]))
 
-# Python2R
-with localconverter(anndata2ri.converter):
-    globalenv["sce"] = adata
+# # Python2R
+# with localconverter(anndata2ri.converter):
+#     globalenv["sce"] = adata
