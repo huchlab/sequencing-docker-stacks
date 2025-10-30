27ad1a27b8c8976dd1b11d0f325068985775c446
Fabian Rost <rost@mpi-cbg.de>
2025-10-30 17:01:29 +0000

    Add singlecell-gpu-notebook to image hierarchy

--- tagging/hierarchy/images_hierarchy.py
+++ tagging/hierarchy/images_hierarchy.py
@@ -80,6 +80,7 @@ ALL_IMAGES = {
     "sequencing-base-notebook": ImageDescription(parent_image="datascience-notebook"),
     "rnaseq-notebook": ImageDescription(parent_image="sequencing-base-notebook"),
     "singlecell-notebook": ImageDescription(parent_image="sequencing-base-notebook"),
+    "singlecell-gpu-notebook": ImageDescription(parent_image="singlecell-notebook"),
     "multiomics-notebook": ImageDescription(parent_image="singlecell-notebook"),
     "spatial-notebook": ImageDescription(parent_image="singlecell-notebook"),
 }
