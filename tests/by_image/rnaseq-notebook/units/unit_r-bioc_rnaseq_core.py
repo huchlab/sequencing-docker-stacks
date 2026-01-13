55167cc6b5048390c48397c226f7a618255eb830
rost <rost@mpi-cbg.de>
2026-01-13 13:29:42 +0000

    update Dockerfiles to install R 4.5 and various Bioconductor packages; add unit tests for new libraries

---
+++ tests/by_image/rnaseq-notebook/units/unit_r-bioc_rnaseq_core.py
@@ -0,0 +1,10 @@
+from rpy2.robjects import r
+
+r("library(apeglm)")
+r("library(BiocParallel)")
+r("library(biomaRt)")
+r("library(clusterProfiler)")
+r("library(GLMGamPoi)")
+r("library(GSEABase)")
+r("library(GSVA)")
+r("library(Gviz)")
