1ba383efe1b52d0db67fa7b8695a5ee142e95b12
rost <rost@mpi-cbg.de>
2026-01-19 10:48:21 +0000

    refactor unit_harmony.py to work with v0.2.0

--- tests/by_image/singlecell-notebook/units/unit_harmony.py
+++ tests/by_image/singlecell-notebook/units/unit_harmony.py
@@ -3,21 +3,26 @@
 See https://github.com/slowkow/harmonypy/issues/24
 """
 
+import harmonypy as hm
 import numpy as np
 import scanpy as sc
 
 adata = sc.datasets.blobs()
 
 sc.pp.pca(adata)
-sc.external.pp.harmony_integrate(adata, "blobs")
 
-X_harmony = adata.obsm["X_pca_harmony"].copy()
+harmony_out = hm.run_harmony(adata.obsm["X_pca"], adata.obs, "blobs")
+
+X_harmony = harmony_out.Z_corr
+
+adata.obsm["X_pca_harmony"] = X_harmony.copy()
 
 for i in range(2):
     print(i)
-    sc.external.pp.harmony_integrate(adata, "blobs")
+    harmony_out = hm.run_harmony(adata.obsm["X_pca"], adata.obs, "blobs")
+    X_harmony = harmony_out.Z_corr
     try:
-        np.testing.assert_equal(X_harmony, adata.obsm["X_pca_harmony"])
+        np.array_equal(X_harmony, adata.obsm["X_pca_harmony"])
     except AssertionError as exc:
         max_diff = np.abs(X_harmony - adata.obsm["X_pca_harmony"]).max()
         raise AssertionError(
