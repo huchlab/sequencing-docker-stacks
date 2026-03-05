6ef173c57ec9ebe476cfde9ef2ba0e77b1405b4a
Fabian Rost <rost@mpi-cbg.de>
2026-03-05 13:30:30 +0100

    Update unit_spatialdata.py

--- tests/by_image/spatial-notebook/units/unit_spatialdata.py
+++ tests/by_image/spatial-notebook/units/unit_spatialdata.py
@@ -5,8 +5,8 @@ current_version = spatialdata.__version__
 required_version = "0.7.2"
 
 # Compare versions logically
-assert version.parse(current_version) > version.parse(
+assert version.parse(current_version) >= version.parse(
     required_version
-), f"spatialdata version {current_version} is not higher than {required_version}"
+), f"spatialdata version {current_version} is not higher or equal than {required_version}"
 
 print(f"Check passed! Current version: {current_version}")
